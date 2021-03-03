accfe7f656ab9806c68c869b8e10a5e3c1b67029
karamazov85 <farkas.adam.mail@gmail.com>
2021-03-03 10:15:47 +0000

    README update

--- src/navbar/menuItems.js
+++ src/navbar/menuItems.js
@@ -1,36 +1,36 @@
 export const menuItems = {
     "services": {
         1: {
-            id: 1,
+            id: 11,
             menuName: "Filming",
             url: "/",
             subMenuItems: [
                 {
-                    id: 1,
+                    id: 111,
                     subMenuName: "Weddings",
                     url: "/",
                     description: "Discover how we make wedding videos."
                 },
                 {
-                    id: 2,
+                    id: 112,
                     subMenuName: "Documentaries",
                     url: "/",
                     description: "Discover how we make documentaries."
                 },
                 {
-                    id: 3,
+                    id: 113,
                     subMenuName: "Interviews",
                     url: "/",
                     description: "Discover how we make interviews."
                 },
                 {
-                    id: 4,
+                    id: 114,
                     subMenuName: "Commercials",
                     url: "/",
                     description: "Discover how we make great commercials."
                 },
                 {
-                    id: 5,
+                    id: 115,
                     subMenuName: "Features",
                     url: "/",
                     description: "Discover how we make great commercials."
@@ -38,36 +38,36 @@ export const menuItems = {
             ]
         },
         2: {
-            id: 2,
+            id: 12,
             menuName: "Editing",
             url: "/",
             subMenuItems: [ 
                 {
-                    id: 1,
+                    id: 121,
                     subMenuName: "Features",
                     url: "/",
                     description: "Discover how we eat chips."
                 },
                 {
-                    id: 2,
+                    id: 122,
                     subMenuName: "Commercials",
                     url: "/",
                     description: "Discover how we catch fish with sticks."
                 },
                 {
-                    id: 3,
+                    id: 123,
                     subMenuName: "Documentaries",
                     url: "/",
                     description: "Discover how we make films that look like documentaries."
                 },
                 {
-                    id: 4,
+                    id: 124,
                     subMenuName: "Weddings",
                     url: "/",
                     description: "We like dringing at weddings. You?"
                 },
                 {
-                    id: 5,
+                    id: 125,
                     subMenuName: "Interviews",
                     url: "/",
                     description: "Do not go gently into that good night."
@@ -76,78 +76,78 @@ export const menuItems = {
             }
     },
     "pricing" : {
-        1: {
-            id: 3,
+        3: {
+            id: 31,
             menuName: "Wedding photography",
             url: "/",
             description: "See our wedding photography prices",
             subMenuItems: [
                 {
-                    id: 1,
+                    id: 311,
                     subMenuName: "Greek Weddings",
                     url: "/",
                     description: "Discover how we price wedding photos for Greeks."
                 },
                 {
-                    id: 2,
+                    id: 312,
                     subMenuName: "Italian Weddings",
                     url: "/",
                     description: "Discover how we price wedding photos for Italians."
                 },
                 {
-                    id: 3,
+                    id: 313,
                     subMenuName: "American Weddings",
                     url: "/",
                     description: "Discover how we price wedding photos for Americans."
                 },
             ]
         }, 
-        2: {
-            id: 4,
+        4: {
+            id: 41,
             menuName: "Landscape photography",
             url: "/",
             description: "See landscape photography prices",
             subMenuItems: [
                 {
-                    id: 1,
+                    id: 411,
                     subMenuName: "Mountain views",
                     url: "/",
                     description: "Discover how we price Mountain photography."
                 },
                 {
-                    id: 2,
+                    id: 412,
                     subMenuName: "Underwater",
                     url: "/",
                     description: "Discover how we price underwater photography."
                 },
                 {
-                    id: 3,
+                    id: 413,
                     subMenuName: "Polar",
                     url: "/",
                     description: "Discover how we price photography of polar expeditions."
                 },
             ],
         }, 
-        3: {
-            id: 5,
+        5: {
+            id: 51,
             menuName: "portrait photography",
             url: "/",
             description: "See posrtrait photography prices",
             subMenuItems: [
                 {
-                    id: 1,
+                    id: 511,
                     subMenuName: "Famous people",
                     url: "/",
                     description: "Discover what we charge for reprints of our photos of famous people."
                 },
                 {
-                    id: 2,
+                    id: 512,
                     subMenuName: "Charity",
                     url: "/",
                     description: "We don't charge a dime for photos we make of poor people."
                 },
                 {
-                    id: 3,
+                    id: 513,
                     subMenuName: "Ordinary people",
                     url: "/",
                     description: "Discover how we price a portrait photo session."
@@ -157,19 +157,19 @@ export const menuItems = {
     },
     "about" : {
         6: {
-            id: 6,
+            id: 61,
             menuName: "Our story",
             url: "/",
             description: "How it all started",
         },
         7: {
-            id: 7,
+            id: 71,
             menuName: "Locations",
             url: "/",
             description: "Where to find us",
         },
         8: {
-            id: 9,
+            id: 91,
             menuName: "Contact",
             url: "/",
             description: "somecreativecompany@gmail.com"
